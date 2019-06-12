677dd8d9a9bd5eb9875d05e6999282432aa42be0
Angela Lo <angelawenlo@gmail.com>
2019-06-12 19:02:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,16 +3,4 @@ class Dog
     @breed = breed
     @name = name
   end
-  def breed=(breed)
-    @breed = breed
-  end
-  def breed
-    @breed
-  end
-  def name=(name)
-    @name = name
-  end
-  def name
-    @name
-  end
 end
